1fb285f4de82fe8d486e114c40caa1f750015fea
seonmin1 <iseonmin161@gmail.com>
2024-11-13 17:31:35 +0900

    프로젝트 기본 구성

---
+++ src/main/java/org/koreait/global/libs/Utils.java
@@ -0,0 +1,31 @@
+package org.koreait.global.libs;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class Utils {
+
+    private final HttpServletRequest request;
+
+    public boolean isMobile() {
+
+        // 요청 헤더 - User-Agent 브라우저 정보
+        String ua = request.getHeader("User-Agent");
+        String pattern = ".*(iPhone|iPod|iPad|BlackBerry|Android|Windows CE|LG|MOT|SAMSUNG|SonyEricsson).*";
+
+        return ua.matches(pattern);
+    }
+
+    /**
+     * mobile, front 템플릿 분리 함수
+     * - mobile 이면 mobile, pc 이면 front
+     */
+    public String tpl(String path) {
+        String prefix = isMobile() ? "mobile" : "front";
+
+        return String.format("%s/%s", prefix, path);
+    }
+}
