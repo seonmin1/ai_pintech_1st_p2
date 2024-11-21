5008d6a5683579a64ffcaf1c2831cd76f9b55fa9
seonmin1 <https://github.com/seonmin1/ai_pintech_1st_p2.git>
2024-11-21 16:12:12 +0900

    예외페이지 처리

---
+++ src/main/java/org/koreait/global/exceptions/CommonException.java
@@ -0,0 +1,22 @@
+package org.koreait.global.exceptions;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.http.HttpStatus;
+
+import java.util.Objects;
+
+/**
+ * 모든 사용자 정의 예외 상위 클래스
+ */
+@Getter @Setter
+public class CommonException extends RuntimeException {
+
+    private HttpStatus status; // 응답 상태
+    private boolean errorCode; // 에러 코드
+
+    public CommonException(String message, HttpStatus status) {
+        super(message);
+        this.status = Objects.requireNonNullElse(status, HttpStatus.INTERNAL_SERVER_ERROR); // null 일 때 기본값 설정
+    }
+}
--- src/main/java/org/koreait/member/controllers/RequestJoin.java
+++ src/main/java/org/koreait/member/controllers/RequestJoin.java
@@ -3,6 +3,7 @@ package org.koreait.member.controllers;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 import org.koreait.member.constants.Gender;
 import org.springframework.format.annotation.DateTimeFormat;
@@ -25,6 +26,7 @@ public class RequestJoin extends RequestAgree {
     private String name; // 회원명
 
     @NotBlank
+    @Size(min = 8) // 비밀번호 8자리 이상
     private String password; // 비밀번호
 
     @NotBlank
