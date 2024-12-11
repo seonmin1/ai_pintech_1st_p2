03c38d1ad6af2650d8168ccd3d0c05d341b7861c
seonmin1 <https://github.com/seonmin1/ai_pintech_1st_p2.git>
2024-12-11 12:21:59 +0900

    포켓몬도감 페이지 구성

--- src/test/java/org/koreait/pokemon/tests/ApiTest2.java
+++ src/test/java/org/koreait/pokemon/tests/ApiTest2.java
@@ -5,6 +5,9 @@ import org.koreait.pokemon.api.services.ApiUpdateService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+/**
+ * 실제 DB에 추가(저장)하는 테스트이므로 환경변수는 포켓몬쪽과 동일하게 설정해줘야 함!
+ */
 @SpringBootTest
 public class ApiTest2 {
     @Autowired
@@ -12,7 +15,7 @@ public class ApiTest2 {
 
     @Test
     void updateTest1() {
-        service.update(1); // 100개
-        service.update(2);
+        service.update(3); // 100개
+        service.update(4);
     }
 }
