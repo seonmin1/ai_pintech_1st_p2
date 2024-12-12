5327f3aa424d4aed7ede09983cf26d11ef4e6e46
seonmin1 <https://github.com/seonmin1/ai_pintech_1st_p2.git>
2024-12-12 10:39:08 +0900

    python test

--- src/main/java/org/koreait/dl/services/PredictService.java
+++ src/main/java/org/koreait/dl/services/PredictService.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Service;
 
 import java.io.BufferedReader;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.List;
 import java.util.stream.Collectors;
 
