e48d68b1e37deca1abc3527afae7f81cd5f98d71
seonmin1 <https://github.com/seonmin1/ai_pintech_1st_p2.git>
2024-12-20 16:05:18 +0900

    포켓몬 찜하기 기능 구현

--- src/main/java/org/koreait/mypage/controllers/MypageController.java
+++ src/main/java/org/koreait/mypage/controllers/MypageController.java
@@ -4,14 +4,20 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.koreait.global.annotations.ApplyErrorPage;
 import org.koreait.global.libs.Utils;
+import org.koreait.global.paging.CommonSearch;
+import org.koreait.global.paging.ListData;
+import org.koreait.global.paging.Pagination;
 import org.koreait.member.MemberInfo;
 import org.koreait.member.entities.Member;
 import org.koreait.member.libs.MemberUtil;
 import org.koreait.member.services.MemberInfoService;
 import org.koreait.member.services.MemberUpdateService;
 import org.koreait.mypage.validators.ProfileValidator;
+import org.koreait.pokemon.controllers.PokemonSearch;
+import org.koreait.pokemon.entities.Pokemon;
+import org.koreait.pokemon.services.PokemonInfoService;
+import org.koreait.wishlist.constants.WishType;
 import org.modelmapper.ModelMapper;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
@@ -22,6 +28,7 @@ import java.security.Principal;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 
 @Controller
 @ApplyErrorPage
@@ -35,6 +42,7 @@ public class MypageController {
     private final MemberUpdateService updateService;
     private final ProfileValidator profileValidator;
     private final MemberInfoService infoService;
+    private final PokemonInfoService pokemonInfoService;
 
     @ModelAttribute("profile")
     public Member getMember() {
@@ -100,6 +108,29 @@ public class MypageController {
     }
 
     /**
+     * 찜하기 목록
+     * @param mode : POKEMON -> 포켓몬 찜하기 목록, BOARD -> 게시글 찜하기 목록
+     *             mode 값에 따라 출력 화면 다름, mode 값 없이 wishlist 만 입력해도 화면 출력
+     */
+    @GetMapping({"/wishlist", "/wishlist/{mode}"})
+    public String wishlist(@PathVariable(name = "mode", required = false) WishType mode, CommonSearch search, Model model) {
+        commonProcess("wishlist", model);
+
+        mode = Objects.requireNonNullElse(mode, WishType.POKEMON);
+
+        if (mode == WishType.BOARD) { // 게시글 찜하기 목록
+
+        } else { // 포켓몬 찜하기 목록
+            PokemonSearch pSearch = modelMapper.map(search, PokemonSearch.class);
+            ListData<Pokemon> data = pokemonInfoService.getMyPokemons(pSearch);
+            model.addAttribute("items", data.getItems());
+            model.addAttribute("pagination", data.getPagination());
+        }
+
+        return utils.tpl("mypage/wishlist");
+    }
+
+    /**
      * 컨트롤러 공통 처리 영역
      */
     private void commonProcess(String mode, Model model) {
@@ -114,6 +145,10 @@ public class MypageController {
             addCommonScript.add("address");
             addScript.add("mypage/profile");
             pageTitle = utils.getMessage("회원정보_수정");
+
+        } else if (mode.equals("wishlist")) { // 찜하기 목록
+            addCommonScript.add("wish");
+            pageTitle = utils.getMessage("♥MY_WISHLIST♥");
         }
 
         model.addAttribute("addCommonScript", addCommonScript);
